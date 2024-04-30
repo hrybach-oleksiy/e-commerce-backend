9246c9ad442fed9cf481da52ba905b3183612290
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-01 00:05:49 +0300

    feat: update User model
    add required fields to the User schema to meet the task requirements

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const addressSchema = require('../schemas/addressSchema');
 
 const userSchema = new Schema({
   email: {
@@ -12,7 +13,32 @@ const userSchema = new Schema({
     required: true,
     trim: true,
   },
-
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  shippingAddress: [
+    {
+      type: addressSchema,
+      required: true,
+    },
+  ],
+  billingAddress: [
+    {
+      type: addressSchema,
+      required: true,
+    },
+  ],
   isActivated: { type: Boolean, default: false },
   activationLink: { type: String },
 });
---
+++ schemas/addressSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isDefault: { type: Boolean, default: false },
+});
+
+module.exports = addressSchema;
