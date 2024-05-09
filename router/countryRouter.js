cc6f3f460a8fb6e18698b91165ad67c8eda814bc
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-10 00:31:12 +0300

    feat: add swagger to the project

--- router/countryRouter.js
+++ router/countryRouter.js
@@ -3,6 +3,23 @@ const countryController = require('../controllers/country-controller');
 
 const countryRouter = express.Router();
 
+/**
+ * @swagger
+ * /countries:
+ *   get:
+ *     summary: Get the list of all countries.
+ *     description: Get the list of all countries.
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of the list of countries.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Country'
+ */
+
 countryRouter.route('/').get(countryController.getCountries);
 
 module.exports = countryRouter;
