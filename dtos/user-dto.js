3ed841fec96a8ba7dc718d76a75c13a042c38fdf
Katerina Rubchenko <97846701+kat2709@users.noreply.github.com>
2024-05-26 13:51:46 +0300

    feat: update user DTO (#10)

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -4,5 +4,26 @@ module.exports = class UserDto {
     this.email = model.email;
     this.id = model._id;
     this.isActivated = model.isActivated;
+    this.firstName = model.firstName;
+    this.lastName = model.lastName;
+    this.dateOfBirth = model.dateOfBirth;
+    this.addresses = {
+      shippingAddresses: model.addresses.shippingAddresses.map((addr) => ({
+        street: addr.street,
+        city: addr.city,
+        postalCode: addr.postalCode,
+        country: addr.country,
+        isDefault: addr.isDefault,
+        id: addr._id,
+      })),
+      billingAddresses: model.addresses.billingAddresses.map((addr) => ({
+        street: addr.street,
+        city: addr.city,
+        postalCode: addr.postalCode,
+        country: addr.country,
+        isDefault: addr.isDefault,
+        id: addr._id,
+      })),
+    };
   }
 };
