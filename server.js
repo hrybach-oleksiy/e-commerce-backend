dfd13f5c91598451b8ce6e2e456e58181806ac1e
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-20 22:14:51 +0300

    feat: add promo codes description

--- server.js
+++ server.js
@@ -13,30 +13,6 @@ const resolveRefs = require('json-refs').resolveRefs;
 
 const PORT = process.env.PORT || 3000;
 const { DB_URL } = process.env;
-// const app = express();
-
-// const swaggerDocument = YAML.load('./docs/swagger.yaml');
-
-// app.use(express.json({ limit: '1mb' }));
-
-// app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-
-// app.use(cookieParser());
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use(
-//   cors({
-//     origin: ['https://hrybach-oleksiy.github.io', 'http://localhost:5173', 'https://playoffthecuff.github.io'],
-//     credentials: true,
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     optionsSuccessStatus: 204,
-//   }),
-// );
-// app.use('/api', router);
-// app.use((req, res, next) => {
-//   res.setHeader('Permissions-Policy', 'interest-cohort=()');
-//   next();
-// });
-// app.use(errorMiddleware);
 
 /**
  * Return JSON with resolved references
