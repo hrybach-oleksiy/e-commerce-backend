3a2988f46ae60646f806961bc64448b2a30c38d2
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-20 21:44:54 +0300

    refactor: split swagger to the components

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const http = require('http');
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -7,33 +9,93 @@ const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
+const resolveRefs = require('json-refs').resolveRefs;
 
 const PORT = process.env.PORT || 3000;
 const { DB_URL } = process.env;
-const app = express();
+// const app = express();
 
-const swaggerDocument = YAML.load('./docs/swagger.yaml');
+// const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
-app.use(express.json({ limit: '1mb' }));
+// app.use(express.json({ limit: '1mb' }));
 
-app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+// app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
-app.use(cookieParser());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(
-  cors({
-    origin: ['https://hrybach-oleksiy.github.io', 'http://localhost:5173', 'https://playoffthecuff.github.io'],
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    optionsSuccessStatus: 204,
-  }),
-);
-app.use('/api', router);
-app.use((req, res, next) => {
-  res.setHeader('Permissions-Policy', 'interest-cohort=()');
-  next();
-});
-app.use(errorMiddleware);
+// app.use(cookieParser());
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use(
+//   cors({
+//     origin: ['https://hrybach-oleksiy.github.io', 'http://localhost:5173', 'https://playoffthecuff.github.io'],
+//     credentials: true,
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     optionsSuccessStatus: 204,
+//   }),
+// );
+// app.use('/api', router);
+// app.use((req, res, next) => {
+//   res.setHeader('Permissions-Policy', 'interest-cohort=()');
+//   next();
+// });
+// app.use(errorMiddleware);
+
+/**
+ * Return JSON with resolved references
+ * @param {array | object} root - The structure to find JSON References within (Swagger spec)
+ * @returns {Promise.<JSON>}
+ */
+const multiFileSwagger = (root) => {
+  const options = {
+    filter: ['relative', 'remote'],
+    loaderOptions: {
+      processContent: function (res, callback) {
+        callback(null, YAML.parse(res.text));
+      },
+    },
+  };
+
+  return resolveRefs(root, options).then(
+    function (results) {
+      return results.resolved;
+    },
+    function (err) {
+      console.log(err.stack);
+    },
+  );
+};
+
+const createServer = async () => {
+  const app = express();
+
+  const swaggerDocument = await multiFileSwagger(YAML.load(path.resolve(__dirname, './docs/swagger.yaml')));
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  app.use(
+    cors({
+      origin: ['https://hrybach-oleksiy.github.io', 'http://localhost:5173', 'https://playoffthecuff.github.io'],
+      credentials: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      optionsSuccessStatus: 204,
+    }),
+  );
+
+  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+  app.use(cookieParser());
+
+  app.use('/api', router);
+
+  app.use((req, res, next) => {
+    res.setHeader('Permissions-Policy', 'interest-cohort=()');
+    next();
+  });
+
+  app.use(errorMiddleware);
+
+  const server = http.createServer(app);
+
+  return server;
+};
 
 const start = async () => {
   try {
@@ -42,7 +104,10 @@ const start = async () => {
     }
 
     await mongoose.connect(DB_URL);
-    app.listen(PORT, () => {
+
+    const server = await createServer();
+
+    server.listen(PORT, () => {
       console.log(`Server starts on port ${PORT}`, `with the collection ${mongoose.connection.name}`);
     });
   } catch (error) {
