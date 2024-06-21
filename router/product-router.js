71c910135946bd4a59dea344b9b79205596e6bd0
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-21 21:52:13 +0300

    fix: change get filters data method in router

--- router/product-router.js
+++ router/product-router.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 productRouter.route('/').post(productController.getProducts);
 productRouter.route('/').get(productController.getProduct);
 productRouter.route('/best-selling').get(productController.getBestSellingProducts);
-productRouter.route('/filters').post(productController.getFiltersData);
+productRouter.route('/filters').get(productController.getFiltersData);
 productRouter.route('/short').get(productController.getShortInfo);
 productRouter.route('/thumb').post(productController.addThumbnail);
 productRouter.route('/gallery').post(productController.addImgToGallery);
